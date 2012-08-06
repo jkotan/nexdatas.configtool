07a235c43b267b25bf8284395f96b5d13b95af43
jkotan <jankotan@gmail.com>
2012-08-06 14:51:34 +0200

    changing .pyw to .py in doxygen comments

--- ndtsconfigtool/AttributeDlg.py
+++ ndtsconfigtool/AttributeDlg.py
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with nexdatas.  If not, see <http://www.gnu.org/licenses/>.
 ## \package ndtsconfigtool nexdatas
-## \file AttributeDlg.pyw
+## \file AttributeDlg.py
 # Attribute dialog class
 
 import re
--- ndtsconfigtool/LinkDlg.py
+++ ndtsconfigtool/LinkDlg.py
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with nexdatas.  If not, see <http://www.gnu.org/licenses/>.
 ## \package ndtsconfigtool nexdatas
-## \file LinkDlg.pyw
+## \file LinkDlg.py
 # Link dialog class
 
 import re

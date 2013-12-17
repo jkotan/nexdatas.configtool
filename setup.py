90c79565d2bb247fe28b9451f102ed50d48ac052
Jan Kotanski <jankotan@gmail.com>
2013-12-17 17:21:14 +0100

    changing debian control

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ class toolClean(clean):
 
 ## metadata for distutils
 SETUPDATA = dict(
-    name = "nexdatas.configtool",
+    name = "python-nxsconfigtool",
     version = ITOOL.__version__,
     author = "Jan Kotanski, Eugen Wintersberger , Halil Pasic",
     author_email = "jankotan@gmail.com, eugen.wintersberger@gmail.com, " \

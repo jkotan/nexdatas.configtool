a71a3924d3a5c5a119e09b22232bdc0e715738c2
Jan Kotanski <jankotan@gmail.com>
2016-03-23 16:26:44 +0100

    add new files

---
+++ nxsconfigtool/ComponentCreator.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+#   This file is part of nexdatas - Tango Server for NeXus data writer
+#
+#    Copyright (C) 2012-2016 DESY, Jan Kotanski <jkotan@mail.desy.de>
+#
+#    nexdatas is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    nexdatas is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with nexdatas.  If not, see <http://www.gnu.org/licenses/>.
+## \package nxsconfigtool nexdatas
+## \file ComponentCreator.py
+# Class for connecting to configuration server
+
+""" Provides connects to configuration server"""
+
+from PyQt4.QtGui import (QMessageBox)
+
+import logging
+## message logger
+logger = logging.getLogger(__name__)
+
+try:
+    import nxstools
+    from nxstools.nxscreator import OnlineCPCreator
+    NXSTOOLS_AVAILABLE = True
+    NXSTOOLS_MASTERVER, NXSTOOLS_MINORVER, NXSTOOLS_PATCHVER = \
+        nxstools.__version__.split(".")
+    if int(NXSTOOLS_MASTERVER) <= 1:
+        if int(NXSTOOLS_MINORVER) <= 18:
+            if int(NXSTOOLS_PATCHVER) <= 3:
+                raise ImportError("nxstools is below 1.18.4")
+except ImportError, e:
+    NXSTOOLS_AVAILABLE = False
+    logger.info("nxstools is not available: %s" % e)
+
+import time
+
+from PyQt4.QtGui import (QFileDialog)
+
+from .CreatorDlg import CreatorDlg
+
+
+## option class
+class Options(object):
+    def __init__(self, server=None):
+        self.server = server
+        self.overwrite = True
+        self.lower = True
+        self.component = None
+
+
+## configuration server
+class ComponentCreator(object):
+
+    ## constructor
+    def __init__(self, configServer, parent):
+        ## configuration server
+        self.configServer = configServer
+
+        ## online file name
+        self.onlineFile = None
+
+        ## componentName
+        self.componentName = None
+
+        ## created components
+        self.components = {}
+        ## created components
+        self.datasources = {}
+        ## available components
+        self.avcomponents = []
+        ## parent
+        self.parent = parent
+
+    ## sets onlineFile name and check if online file exists
+    def checkOnlineFile(self, onlineFile):
+        onlineFile = onlineFile or '/online_dir/online.xml'
+        onlineFile = unicode(QFileDialog.getOpenFileName(
+            self.parent, "Open Online File", onlineFile,
+            "XML files (*.xml)"))
+        if onlineFile:
+            self.onlineFile = onlineFile
+            return True
+        else:
+            self.onlineFile = None
+
+    ## creates component and datasources from online xml
+    def create(self):
+        self.action = None
+        self.componentName = None
+        self.components = {}
+        self.datasources = {}
+        if NXSTOOLS_AVAILABLE and self.onlineFile:
+            options = Options(self.configServer.getDeviceName())
+
+            occ = OnlineCPCreator(options, [self.onlineFile], False)
+            self.avcomponents = occ.listcomponents() or []
+            if self.avcomponents:
+                self.action = self.selectComponent()
+                options.component = self.componentName
+                if self.action:
+                    occ.createXMLs()
+                    self.components = dict(occ.components)
+                    self.datasources = dict(occ.datasources)
+            else:
+                QMessageBox.warning(
+                    self.parent,
+                    "Error in creating Component",
+                    "Cannot find any known components in '%s'" %
+                    self.onlineFile)
+
+    ## runs Creator widget to select the required component
+    # \returns action to be performed with the components and datasources
+    def selectComponent(self):
+        aform = CreatorDlg(self.parent)
+        if self.avcomponents:
+            aform.components = list(self.avcomponents)
+
+        action = None
+        self.componentName = None
+        aform.createGUI()
+        aform.show()
+        if aform.exec_():
+            action = aform.action
+            self.componentName = aform.componentName
+        return action
+
+
+## test function
+def test():
+    import sys
+    from PyQt4.QtGui import QApplication
+
+    app = QApplication(sys.argv)
+#    cs = ComponentCreator()
+#    cs.onlineFile = "/online_dir/online.xml"
+#    cs.open()
+#    cs.close()
+    app.exit()
+
+if __name__ == "__main__":
+    test()
---
+++ nxsconfigtool/CreatorDlg.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+#   This file is part of nexdatas - Tango Server for NeXus data writer
+#
+#    Copyright (C) 2012-2016 DESY, Jan Kotanski <jkotan@mail.desy.de>
+#
+#    nexdatas is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    nexdatas is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with nexdatas.  If not, see <http://www.gnu.org/licenses/>.
+## \package nxsconfigtool nexdatas
+## \file CreatorDlg.py
+# Component Creator dialog class
+
+""" server creator widget """
+
+from PyQt4.QtCore import SIGNAL
+from PyQt4.QtGui import (QDialog, QMessageBox)
+
+from .ui.ui_creatordlg import Ui_CreatorDlg
+
+import logging
+## message logger
+logger = logging.getLogger(__name__)
+
+
+## dialog defining a component creator dialog
+class CreatorDlg(QDialog):
+
+    ## constructor
+    # \param parent patent instance
+    def __init__(self, parent=None):
+        super(CreatorDlg, self).__init__(parent)
+
+        ## host name of the configuration server
+        self.components = []
+        self.componentName = None
+        ## user interface
+        self.ui = Ui_CreatorDlg()
+        self.action = ''
+
+    ## creates GUI
+    # \brief It updates GUI and creates creatorion for required actions
+    def createGUI(self):
+        self.ui.setupUi(self)
+        self.updateForm()
+
+        self.connect(self.ui.savePushButton, SIGNAL("clicked()"),
+                     self.savePressed)
+        self.connect(self.ui.storePushButton, SIGNAL("clicked()"),
+                     self.storePressed)
+        self.connect(self.ui.applyPushButton, SIGNAL("clicked()"),
+                     self.applyPressed)
+        self.connect(self.ui.cancelPushButton, SIGNAL("clicked()"),
+                     self.reject)
+
+    ## updates the connect dialog
+    # \brief It sets initial values of the connection form
+    def updateForm(self):
+        self.ui.compComboBox.clear()
+        if self.components:
+            self.ui.compComboBox.addItems(self.components)
+
+    def savePressed(self):
+        self.componentName = unicode(self.ui.compComboBox.currentText())
+        self.action = 'SAVE'
+        QDialog.accept(self)
+
+    def storePressed(self):
+        self.componentName = unicode(self.ui.compComboBox.currentText())
+        self.action = 'STORE'
+        QDialog.accept(self)
+
+    def applyPressed(self):
+        self.componentName = unicode(self.ui.compComboBox.currentText())
+        self.action = 'APPLY'
+        QDialog.accept(self)
+
+
+if __name__ == "__main__":
+    import sys
+    from PyQt4.QtGui import QApplication
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    ## Qt application
+    app = QApplication(sys.argv)
+    ## connect form
+    form = CreatorDlg()
+    form.createGUI()
+    form.show()
+    app.exec_()
+
+    if form.result():
+        if form.device:
+            logger.info("Connect: %s , %s , %s" %
+                        (form.device, form.host, form.port))

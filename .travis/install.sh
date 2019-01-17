cf0b76f3fb0048f82d02126fb10bdda682430f02
Jan Kotanski <jankotan@gmail.com>
2019-01-17 20:00:33 +0100

    update test dependences

--- .travis/install.sh
+++ .travis/install.sh
@@ -39,7 +39,7 @@ fi
 
 
 echo "install sardana, taurus and nexdatas"
-docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update;  apt-get -qq install -y  nxsconfigserver-db; sleep 10; apt-get -qq install -y   python-nxsconfigserver python-nxstools pyqt4-dev-tools python-qt5 python-setuptools pyqt5-dev-tools'
+docker exec -it --user root ndts /bin/sh -c 'export DEBIAN_FRONTEND=noninteractive; apt-get -qq update;  apt-get -qq install -y  nxsconfigserver-db; sleep 10; apt-get -qq install -y   python-nxsconfigserver python-nxstools python-pyqt5 python-setuptools pyqt5-dev-tools'
 if [ $? -ne "0" ]
 then
     exit -1
